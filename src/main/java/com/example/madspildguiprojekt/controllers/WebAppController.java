eeb64caf5352beff126e777861cc7bb60a1b139d
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2022-02-28 13:26:29 +0100

    ddd

--- src/main/java/com/example/madspildguiprojekt/controllers/WebAppController.java
+++ src/main/java/com/example/madspildguiprojekt/controllers/WebAppController.java
@@ -4,11 +4,23 @@ import jdk.jfr.Category;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.util.Scanner;
+
 @Controller
 public class WebAppController {
+
+    public WebAppController() {
+    }
 
     @GetMapping("/")
     public String index(){
         return "index";
+    }
+
+    @GetMapping("/counter")
+    public String counter(){
+        return "counter";
     }
 }
