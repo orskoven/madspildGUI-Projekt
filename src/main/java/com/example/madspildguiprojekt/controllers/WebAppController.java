433d88a4cd0822549790c30b17a3b688f3963fe8
Simon Pedersen <90264457+orskoven@users.noreply.github.com>
2022-03-03 14:42:59 +0100

    FWEFWEFFEW

--- src/main/java/com/example/madspildguiprojekt/controllers/WebAppController.java
+++ src/main/java/com/example/madspildguiprojekt/controllers/WebAppController.java
@@ -1,6 +1,5 @@
 package com.example.madspildguiprojekt.controllers;
 
-import com.example.madspildguiprojekt.controllers.repositories.Greeting;
 import jdk.jfr.Category;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
